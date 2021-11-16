cbbbaf30f8e29ca064726f141e9efe5ef2f7e35d
zhangliao <zhangliao@baiwang.com>
2021-11-16 18:06:04 +0800

    feat: 增加keys测试demo

--- tests/es-module-exports/test-package.js
+++ tests/es-module-exports/test-package.js
@@ -3,3 +3,29 @@ const packageConfig = require('./moduleA/package.json')
 console.log(Object.getOwnPropertyNames(packageConfig.exports))
 
 console.log(Object.keys(packageConfig.exports))
+
+
+const testObjA = {
+    "import": "./esm-export.mjs",
+    "require": "./reuire-export.cjs",
+    "node": {
+        "require": "./require-export.cjs",
+        "import": "./esm-export.mjs"
+    },
+    "default": "./main-export.mjs"
+}
+
+const testObjB = {
+    "node": {
+        "require": "./require-export.cjs",
+        "import": "./esm-export.mjs"
+    },
+    "default": "./main-export.mjs",
+    "import": "./esm-export.mjs",
+    "require": "./reuire-export.cjs",
+}
+
+console.log(Object.keys(testObjA))
+console.log(Object.getOwnPropertyNames(testObjA))
+console.log(Object.keys(testObjB))
+console.log(Object.getOwnPropertyNames(testObjB))
